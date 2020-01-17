165a30274f2ea2016ebc2dd2124321175609060c
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2020-01-17 22:02:05 +0100

    leaf is the node

--- src/tree/leaf.rs
+++ src/tree/leaf.rs
@@ -1,16 +1,13 @@
-#[derive(Copy, Clone,PartialEq,Debug)]
-struct Leaf<T> {
-    pub val: T,
-}
-
-
-struct Node<T> {
+#[derive(Clone)]
+struct Node<T> where T: Clone{
+    pub val: Option<T>,
     pub operator: fn(left:T, right:T) -> T,
     pub left:Option<Box<Node<T>>>,
     pub right:Option<Box<Node<T>>>,
 }
 
-impl<T> Node<T> {
+
+impl<T> Node<T> where T:Clone{
 
     fn insert () -> () {}
 
