069dbed06f8a4e279798d38ab2b0a6b9c297c989
Ionelia Buzatu <buzatuionelia@gmail.com>
2020-01-17 21:28:11 +0100

    where is the end

--- src/tree/leaf.rs
+++ src/tree/leaf.rs
@@ -1,3 +1,26 @@
-struct Leaf<T> {
-    val: T
+struct Node<T> {
+    pub operator: fn(left:T, right:T) -> T,
+    pub left:Option<Node<T>>,
+    pub right:Option<Node<T>>,
 }
+
+impl<T> Building_<T> {
+
+    fn insert () -> () {}
+
+    fn insert_left () -> () {}
+
+    fn insert_right () -> () {}
+
+    fn find () -> () {}
+
+    fn operation () -> () {}
+
+}
+
+
+
+
+
+
+
