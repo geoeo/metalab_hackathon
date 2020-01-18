3ff0169c477940fb37a8d3f6e9bda22e0b300a96
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2020-01-18 15:54:06 +0100

    merge

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub fn run() -> () {
     println!("How are you doing?");
     let how_are_you = String::new();
     let no_int = 'b';
-    io::stdin().read_line(&mut how_are_you).except("Failed to load");
+    //io::stdin().read_line(&mut how_are_you).except("Failed to load");
 
 
 }
